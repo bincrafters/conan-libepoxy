c1edc8186bf3cacbf6982cbc0a8587f794186b98
bincrafters-user <bincrafters@gmail.com>
2020-05-06 04:49:32 +0000

    Update Conan conventions
    Automatically created by bincrafters-conventions 0.24.0

--- conanfile.py
+++ conanfile.py
@@ -46,7 +46,7 @@ class EpoxyConan(ConanFile):
             del self.options.x11
 
     def build_requirements(self):
-        self.build_requires("meson/0.53.2")
+        self.build_requires("meson/0.54.0")
 
     def requirements(self):
         self.requires("opengl/virtual@bincrafters/stable")
